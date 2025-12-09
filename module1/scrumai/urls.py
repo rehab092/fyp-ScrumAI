a3b43e44e364bc6e90fccbf70a90bd1406404234
Uswa <uswa.zafar@groupebia.com>
2025-12-09 23:26:13 +0500

    Clean repo: removed cache files, added .gitignore

--- module1/scrumai/urls.py
+++ module1/scrumai/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userstories/', include('userstorymanager.urls')),
+   
+
     
 ]
 if settings.DEBUG:
